a4ef2a51f6fa26c1ce2694b84e96f2fa23630fd7
Pichers <antoniorafaelalmeida@hotmail.com>
2023-10-02 22:14:45 +0100

    list first methods (list_create, list_add, list_get) and 2 new auxiliar methods (getNode and isEmpty)

--- Projeto1/include/list-private.h
+++ Projeto1/include/list-private.h
@@ -5,12 +5,45 @@
 
 struct node_t {
 	struct entry_t *entry;
-	struct node_t  *next; 
+	struct node_t  *next;
+	struct node_t *previous;
 };
 
 struct list_t {
 	int size;
 	struct node_t *head;
 };
+
+/**
+ * Creates a new empty list
+*/
+struct list_t* makeList();
+
+/**
+ * Returns the head entry of the list
+*/
+struct entry_t* head(struct list_t* l);
+
+/**
+ * Returns the tail of the list
+*/
+struct list_t* tail(struct list_t* l);
+
+/**
+ * Returns the element of the list l with index i
+*/
+struct entry_t* get(struct list_t* l, int i);
+
+/**
+ * Returns 1 if the list l is empty, 0 otherwise
+*/
+int isEmpty(struct list_t* l);
+
+/**
+ * Adds the given entry e to the end of the list l
+*/
+void add(struct list_t* l, struct entry_t* e);
+
+
 
 #endif
---
+++ Projeto1/src/list.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/list-private.h"
+#include "../include/list.h"
+
+struct node_t* getNode(struct list_t* l, char* k);
+
+/* Função que cria e inicializa uma nova lista (estrutura list_t a
+ * ser definida pelo grupo no ficheiro list-private.h).
+ * Retorna a lista ou NULL em caso de erro.
+ */
+struct list_t *list_create(){
+    struct list_t* list = malloc(sizeof(struct list_t*));
+
+    if(list == NULL){
+        return NULL;
+    }
+    
+    list->size = 0;
+    list->head = NULL;
+
+    return list;
+}
+
+/* Função que elimina uma lista, libertando *toda* a memória utilizada
+ * pela lista.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int list_destroy(struct list_t *list){
+
+}
+
+/* Função que adiciona à lista a entry passada como argumento.
+ * A entry é inserida de forma ordenada, tendo por base a comparação
+ * de entries feita pela função entry_compare do módulo entry e
+ * considerando que a entry menor deve ficar na cabeça da lista.
+ * Se já existir uma entry igual (com a mesma chave), a entry
+ * já existente na lista será substituída pela nova entry,
+ * sendo libertada a memória ocupada pela entry antiga.
+ * Retorna 0 se a entry ainda não existia, 1 se já existia e foi
+ * substituída, ou -1 em caso de erro.
+ */
+int list_add(struct list_t *list, struct entry_t *entry){
+
+    struct node_t* newNode = malloc(sizeof(struct node_t*));
+    newNode->entry = entry;
+
+    if(list == NULL || entry == NULL){
+        return -1;
+    }
+    if(isEmpty(list)){
+        
+        newNode->next = NULL;
+        newNode->previous = NULL;
+
+        list->head = newNode;
+        list->size = 1;
+        return 0;
+    }
+
+    struct node_t* node1 = list->head;
+
+    for (int i = 0; i < list->size; i++){
+
+        if(entry_compare(node1->entry, entry) == -1){
+            
+            newNode->next = node1;
+            newNode->previous = node1->previous;
+
+            if(i == 0){
+                list->head = newNode;
+                return 0;
+            }
+
+            newNode->previous->next = newNode;
+
+            return 0;
+        }
+        if(entry_compare(node1->entry, entry) == 0){
+
+            newNode->next = node1->next;
+            newNode->previous = node1->previous;
+
+            if(i == 0){
+                list->head = newNode;
+                return 1;
+            }
+            newNode->previous->next = newNode;
+
+            entry_destroy(node1->entry);
+            free(node1);
+
+            return 1;
+        }
+        if(node1->next == NULL)
+            break;
+        node1 = node1->next;
+    }
+    newNode->previous = node1;
+    node1->next = newNode;
+
+    return 0;
+}
+
+/* Função que elimina da lista a entry com a chave key, libertando a
+ * memória ocupada pela entry.
+ * Retorna 0 se encontrou e removeu a entry, 1 se não encontrou a entry,
+ * ou -1 em caso de erro.
+ */
+int list_remove(struct list_t *list, char *key){
+    //correr a lista, encontrar o node, prev = next, next.prev = prev, destroy_entry, free node;
+    
+}
+
+/* Função que obtém da lista a entry com a chave key.
+ * Retorna a referência da entry na lista ou NULL se não encontrar a
+ * entry ou em caso de erro.
+*/
+struct entry_t* list_get(struct list_t* list, char* key){
+    struct node_t* n = malloc(sizeof(struct node_t));
+    n = getNode(list, key);
+
+    return n->entry;
+}
+
+/* Função que conta o número de entries na lista passada como argumento.
+ * Retorna o tamanho da lista ou -1 em caso de erro.
+ */
+int list_size(struct list_t *list){
+    return list->size;
+}
+
+/* Função que constrói um array de char* com a cópia de todas as keys na 
+ * lista, colocando o último elemento do array com o valor NULL e
+ * reservando toda a memória necessária.
+ * Retorna o array de strings ou NULL em caso de erro.
+ */
+char **list_get_keys(struct list_t *list){
+
+}
+
+/* Função que liberta a memória ocupada pelo array de keys obtido pela 
+ * função list_get_keys.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int list_free_keys(char **keys){
+
+}
+
+/**
+ * Returns 1 if the list l is empty, 0 if it's not, -1 in case of error
+*/
+int isEmpty(struct list_t* l){
+    if(l == NULL)
+        return -1;
+    
+    if(l->head == NULL && l->size == 0)
+        return 1;
+    if(l->head != NULL && l->size != 0)
+        return 0;
+    return -1;
+}
+
+struct node_t* getNode(struct list_t* l, char* k){
+
+    if(l == NULL || k == NULL)
+        return NULL;
+
+    struct node_t* cNode = l->head;
+
+    for (int i = 0; i < l->size; i++){
+        if(strcmp(cNode->entry->key, k) == 0){
+            return cNode;
+        }
+        if(cNode->next == NULL)
+            break;
+        cNode = cNode->next;
+    }
+    return NULL;
+}
