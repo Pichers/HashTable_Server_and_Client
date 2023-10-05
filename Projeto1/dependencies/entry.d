bbb8bbef10948da925aa28c7ae8772b7402e7a3b
fc58212 <fc58212@alunos.fc.ul.pt>
2023-10-05 23:40:45 +0100

    makefile e um pouco da table

---
+++ Projeto1/dependencies/entry.d
@@ -0,0 +1,5 @@
+obj/entry.o: src/entry.c src/../include/data.h src/../include/entry.h \
+ src/../include/data.h
+src/../include/data.h:
+src/../include/entry.h:
+src/../include/data.h:
---
+++ Projeto1/dependencies/test_list.d
@@ -0,0 +1,11 @@
+obj/test_list.o: ../Projeto1_Testes/test_list.c \
+ ../Projeto1_Testes/../Projeto1/include/list.h \
+ ../Projeto1_Testes/../Projeto1/include/data.h \
+ ../Projeto1_Testes/../Projeto1/include/entry.h \
+ ../Projeto1_Testes/../Projeto1/include/data.h \
+ ../Projeto1_Testes/../Projeto1/include/entry.h
+../Projeto1_Testes/../Projeto1/include/list.h:
+../Projeto1_Testes/../Projeto1/include/data.h:
+../Projeto1_Testes/../Projeto1/include/entry.h:
+../Projeto1_Testes/../Projeto1/include/data.h:
+../Projeto1_Testes/../Projeto1/include/entry.h:
