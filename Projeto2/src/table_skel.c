123cf98542625802707bd1140b45198083ea5231
Pichers <antoniorafaelalmeida@hotmail.com>
2023-10-27 16:30:54 +0100

    maybe table_skel works, maybe have to check for errors or free memory

--- Projeto2/src/table_skel.c
+++ Projeto2/src/table_skel.c
@@ -1,3 +1,7 @@
+#include <stddef.h> 
+#include <stdlib.h>
+#include <string.h>
+
 #include "../include/table.h"
 #include "../sdmessage.pb-c.h"
 
@@ -27,39 +31,50 @@ int table_skel_destroy(struct table_t *table){
     return i;
 }
 
+void handleError(struct MessageT* msg){
+    msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+    msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+
+    return -1;
+}
+
+
 /* Executa na tabela table a operação indicada pelo opcode contido em msg 
  * e utiliza a mesma estrutura MessageT para devolver o resultado.
  * Retorna 0 (OK) ou -1 em caso de erro.
 */
 int invoke(MessageT *msg, struct table_t *table){
-    struct MessageT__Opcode* opCode = msg->opcode;
 
-    int i;
+    if(table == NULL){
+        return handleError(msg);
+    }
+    if(msg == NULL){
+        return handleError(msg);
+    }
+
+    struct MessageT__Opcode* opCode = msg->opcode;
 
     switch(opCode) {
         case MESSAGE_T__OPCODE__OP_PUT:
             
-            msg->opcode = MESSAGE_T__OPCODE__OP_PUT + 1;
+            msg->opcode++;
             msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
 
             char* key = msg->key;
 
-            //???
-            ProtobufCBinaryData msgValue = msg->value;
-            struct data_t* data = data_create(msgValue->len, msgValue->data);
+            struct data_t* data = data_create(msg->value->len, msg->value->data);
 
-            i = table_put(table, key, data);
+            int i = table_put(table, key, data);
             msg->key = NULL;
 
             if(i == -1){
-                msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
-                msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+                return handleError(msg);
             }
-
+            
             break;
         case MESSAGE_T__OPCODE__OP_GET:
 
-            msg->opcode = MESSAGE_T__OPCODE__OP_GET + 1;
+            msg->opcode++;
             msg->c_type = MESSAGE_T__C_TYPE__CT_VALUE;
 
             char* key = msg->key;
@@ -67,66 +82,53 @@ int invoke(MessageT *msg, struct table_t *table){
             struct data_t* dataValue = table_get(table, key);
 
             if(dataValue == NULL){
-                i = -1;
-                msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
-                msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+                return handleError(msg);
             }
             else{
                 msg->value = dataValue;
+                free(msg->entry)
                 msg->key = NULL;
-                i = 0
             }
 
             break;
         case MESSAGE_T__OPCODE__OP_DEL:
             
-            msg->opcode = MESSAGE_T__OPCODE__OP_DEL + 1;
+            msg->opcode++;
             msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
 
             char* key = msg->key;
 
-            i = table_remove(table, key);
+            int i = table_remove(table, key);
 
-            msg->key = NULL
+            free(msg->key);
 
             if(i = -1){
-                msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
-                msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+                return handleError(msg);
             }
 
             break;
         case MESSAGE_T__OPCODE__OP_SIZE:
             
-            msg->opcode = MESSAGE_T__OPCODE__OP_SIZE + 1;
+            msg->opcode++;
             msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
 
             int size = table_size(table);
 
             if(size == -1){
-                msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
-                msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
-
-                i = -1;
+                return handleError(msg);
             }
-            else{
-                msg->result = size;
-                i = 0;
-            }
-
+            msg->result = size;
 
             break;
         case MESSAGE_T__OPCODE__OP_GETKEYS:
             
-            msg->opcode = MESSAGE_T__OPCODE__OP_GETKEYS + 1;
+            msg->opcode++;
             msg->c_type = MESSAGE_T__C_TYPE__CT_KEYS;
 
             char** keys = table_get_keys(table);
 
             if(keys == NULL){
-                msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
-                msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
-
-                i = -1;
+                return handleError(msg);
             }
             else{
                 int j = 0;
@@ -136,23 +138,58 @@ int invoke(MessageT *msg, struct table_t *table){
                                         //
                 msg->n_keys = j;        //não sei se é suposto por isto, mas parece acertado
                 msg->keys = keys;
-                i = 0;
             }
 
             break;
         case MESSAGE_T__OPCODE__OP_GETTABLE:
             
-            msg->opcode = MESSAGE_T__OPCODE__OP_GETKEYS + 1;
+            msg->opcode++;
             msg->c_type = MESSAGE_T__C_TYPE__CT_KEYS;
 
-            //coisinhas (não percebi "EntryT")
+            struct char** keys = table_get_keys(table);
+            
+            if(keys == NULL){
+                return handleError(table, i);
+            }
+
+            int nKeys = 0;
+
+            while(keys[nKeys] != NULL){
+                nKeys++;
+            }
+
+            struct entry_t** entries = malloc(nKeys * sizeof(struct entry_t));
+            if(entries == NULL){
+                return handleError(msg);
+            }
+
+            for(int j = 0; j < nKeys; j++){
+                char* key = keys[j];
+                struct data_t* data = data_dup(table_get(table, keys[j]));
+                if(data == NULL){
+                    return handleError(msg);
+                }
+
+                char* dupKey = strdup(key);
+                if(dupKey == NULL){
+                    return handleError(msg);
+                }
+
+                struct entry_t* entry = malloc(sizeof(struct entry_t));
+                if(entry == NULL){
+                    return handleError(msg);
+                }
+
+                entry = entry_create(dupKey, data);
+                entries[j] = entry;
+            }
+
+            msg->n_entries = nKeys;
+            msg->entries = entries;
 
             break;
         default: // Case BAD || ERROR
-            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
-            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
-
-            i = -1;
+            return handleError(msg);
     }
-    return i;
+    return 0;
 }
