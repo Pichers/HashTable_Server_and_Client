23956e904cd5af406b91f9fac7b1cc65cb888e70
fc58212 <fc58212@1230-14.alunos.di.fc.ul.pt>
2023-10-30 15:37:01 +0000

    make?

---
+++ Projeto2/dependencies/table_server.d
@@ -0,0 +1,8 @@
+obj/table_server.o: src/table_server.c include/table_skel.h \
+ include/table.h include/data.h include/sdmessage.pb-c.h \
+ include/network_server.h
+include/table_skel.h:
+include/table.h:
+include/data.h:
+include/sdmessage.pb-c.h:
+include/network_server.h:
