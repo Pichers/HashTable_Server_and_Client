dd00c5fe6afe6989b4a57ad9f03440e9fb7332dd
Pichers <antoniorafaelalmeida@hotmail.com>
2023-10-30 02:34:22 +0000

    a

--- Projeto2/dependencies/entry.d
+++
@@ -1,4 +0,0 @@
-obj/entry.o: src/entry.c include/data.h include/entry.h include/data.h
-include/data.h:
-include/entry.h:
-include/data.h:
--- Projeto2/dependencies/table_client.d
+++
@@ -1,5 +0,0 @@
-obj/table_client.o: src/table_client.c include/client_stub.h \
- include/data.h include/entry.h
-include/client_stub.h:
-include/data.h:
-include/entry.h:
